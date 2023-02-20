a5e8a2511cae020e646be552cd9d77fbb43cd7aa
Catherine Huang <38745832+catherinehuang82@users.noreply.github.com>
2023-02-20 18:58:58 -0500

    more gRPC server (but issues when we connect multiple clients)

--- grpc_chat/src/server.cc
+++ grpc_chat/src/server.cc
@@ -14,7 +14,6 @@
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
 #include <grpcpp/security/server_credentials.h>
-#include "chat.grpc.pb.h"
 
 using chat::Chat;
 using chat::Message;
@@ -42,26 +41,20 @@ const std::string SERVER_ADDRESS = "0.0.0.0:50051";
 // in our implementation, there is only one chat session where all repicients participate
 class Session
 {
-
-	std::unordered_map<std::string, ServerReaderWriter<StreamResponse, StreamRequest> *> d_allStreams;
+    // perhaps? maybe we need one chat stream per username though
+	// std::shared_ptr<std::string, ServerReaderWriter<StreamResponse, StreamRequest>> chat_stream;
 	std::mutex d_mutex;
-	std::string d_id;
+	// std::string d_user;
 	int d_userCount;
 
 public:
-	Session(const std::string &id) : d_id(id), d_mutex(), d_userCount(0) {}
+    // session constructor
+    // we no longer need a session ID because we assume only one session
+	Session() :  d_mutex(), d_userCount(0) {
+    }
 
-	// Return a modifiable reference to the session id.
-	std::string &id()
-	{
-		return d_id;
-	}
-
-	// Return a non-modifiable reference to the sessions id.
-	const std::string &id() const
-	{
-		return d_id;
-	}
+    std::unordered_map<std::string, ServerReaderWriter<StreamResponse, StreamRequest> *> d_allStreams;
+    std::set<std::string> allAccounts;
 
 	void broadcast(StreamResponse message)
 	{
@@ -71,32 +64,36 @@ public:
 		}
 	}
 
+    void send_message(std::string recipient_username, StreamResponse message) {
+        d_allStreams[recipient_username]->Write(message);
+    }
+
 	void join(ServerReaderWriter<StreamResponse, StreamRequest> *stream, const std::string &username, bool isNewSession = false)
 	{
+        // the isNewSession argument is unused!
 		StreamResponse responseMessage;
-		if (isNewSession)
-		{
-			d_allStreams[username] = stream;
-			++d_userCount;
-			return;
-		}
 
 		if (d_allStreams.find(username) != d_allStreams.end())
+        // if user is already logged in
 		{
-			JoinSessionResponse *join_session_response = new JoinSessionResponse();
-			join_session_response->set_message("Username " + username + " is already taken.");
-			responseMessage.set_allocated_joinsession_response(join_session_response);
+			LoginResponse *failed_login_response = new LoginResponse();
+			failed_login_response->set_message("Username " + username + " is already logged in.");
+			responseMessage.set_allocated_login_response(failed_login_response);
 			stream->Write(responseMessage);
 		}
 		else
 		{
+            std::cout << "Successful login for user: " << username << std::endl;
 			std::lock_guard<std::mutex> lg(d_mutex);
 			LoginResponse *newLogin = new LoginResponse();
 			newLogin->set_username(username);
 			d_allStreams[username] = stream;
+            allAccounts.insert(username);
 			responseMessage.set_allocated_login_response(newLogin);
+            std::cout << "Debug 2" << std::endl;
 			++d_userCount;
 			broadcast(responseMessage);
+            std::cout << "Debug 3" << std::endl;
 		}
 	}
 
@@ -114,19 +111,21 @@ public:
 
 class ChatImpl final : public Chat::Service
 {
-
-	std::unordered_map<std::string, std::unique_ptr<Session>> d_sessions;
+	// std::unordered_map<std::string, std::unique_ptr<Session>> d_sessions;
+    std::unique_ptr<Session> session;
 	std::mutex d_mutex;
 
 public:
 	Status ChatStream(ServerContext *context, ServerReaderWriter<StreamResponse, StreamRequest> *stream) override
 	{
+        std::unique_ptr<Session> newSession(new Session());
+		session = std::move(newSession);
 		StreamRequest message;
 
 		while (stream->Read(&message))
 		{
 			StreamResponse responseMessage;
-			std::string id = "";
+			// std::string id = "";
 			std::string username = "";
 			if (message.has_login_request())
 			{
@@ -135,83 +134,97 @@ public:
 				username = message.login_request().username();
 
 				d_mutex.lock();
-				while (true)
-				{
-					id = generateRandomId();
-					if (d_sessions.find(id) == d_sessions.end())
-					{
-						break;
-					}
-				}
-				std::unique_ptr<Session> newSession(new Session(id));
-				d_sessions[id] = std::move(newSession);
-				d_mutex.unlock();
 
-				StartSessionResponse *start_session_response = new StartSessionResponse();
-				start_session_response->set_session_id(id);
-				responseMessage.set_allocated_startsession_response(start_session_response);
-				d_sessions[id]->join(stream, username, true);
-				d_sessions[id]->broadcast(responseMessage);
+                session->join(stream, username, true);
+                d_mutex.unlock();
+
+				// StartSessionResponse *start_session_response = new StartSessionResponse();
+				// start_session_response->set_session_id(id);
+				// responseMessage.set_allocated_startsession_response(start_session_response);
+                LoginResponse *login_response = new LoginResponse();
+                login_response->set_username(username);
+                responseMessage.set_allocated_login_response(login_response);
+                
+				session->broadcast(responseMessage);
 			}
-			else if (message.has_joinsession_request())
+			else if (message.has_listaccounts_request())
 			{
-				std::cout << "Join session request received\n";
+				std::cout << "List accounts request received\n";
 
-				id = message.joinsession_request().session_id();
-				username = message.joinsession_request().username();
+				// id = message.joinsession_request().session_id();
+				username = message.listaccounts_request().username();
+                std::string wildcard = message.listaccounts_request().wildcard();
 
-				auto session = d_sessions.find(id);
-				if (session != d_sessions.end())
-				{
-					session->second->join(stream, username);
-					continue;
-				}
-				JoinSessionResponse *join_session_response = new JoinSessionResponse();
-				join_session_response->set_message(id + " is not a valid session_id.");
-				responseMessage.set_allocated_joinsession_response(join_session_response);
+                std::string matching_accounts;
+                // iterate through keys (usernames)
+                for (auto it = session->d_allStreams.begin(); it != session->d_allStreams.end(); ++it)
+                {
+                    std::string a = it->first;
+                    // if a username contains wildcard, add to string
+                    if (a.find(wildcard) != std::string::npos) {
+                        matching_accounts += a;
+                        matching_accounts += '\n';
+                    }
+                }
+
+                // accounts_list_str.pop_back();
+                printf("listing accounts...\n");
+
+                // send accounts list to client that requested it
+                ListAccountsResponse *listAccounts_response = new ListAccountsResponse();
+                listAccounts_response->set_username(username);
+                listAccounts_response->set_accounts(matching_accounts);
+
+                // TODO: iterate through all usernames in d_allStreams
+                // and add to the accounts repeated field
+
+				responseMessage.set_allocated_listaccounts_response(listAccounts_response);
 				stream->Write(responseMessage);
 			}
 			else if (message.has_message_request())
 			{
-				std::cout << "New message received\n";
-				username = message.message_request().username();
+				std::cout << "New message received. ";
+				username = message.message_request().sender_username();
+                std::string recipient_username = message.message_request().recipient_username();
 
 				Message *newMessage = new Message();
-				newMessage->set_username(username);
+				newMessage->set_sender_username(username);
+                std::cout << "Sender: " << username;
+                newMessage->set_recipient_username(recipient_username);
+                std::cout << ". Recipient: " << recipient_username;
 				newMessage->set_message(message.message_request().message());
+                std::cout << ". Message: " << message.message_request().message() << std::endl;
 				responseMessage.set_allocated_message_response(newMessage);
 
-				auto session = d_sessions.find(id);
-				if (session == d_sessions.end())
-				{
-					continue;
-				}
-				session->second->broadcast(responseMessage);
+                // recipient session
+
+                d_mutex.lock();
+                session->d_allStreams[recipient_username]->Write(responseMessage);
+                d_mutex.unlock();
+                std::cout << "Debug 1" << std::endl;
+                std::cout << "Debug 4" << std::endl;
 			}
 			else if (message.has_logout_request())
 			{
 				std::cout << "Logout request received\n";
 
 				// id = message.quitsession_request().session_id();
+                // user to be logged out
 				username = message.logout_request().username();
 
 				LogoutResponse *newLogout = new LogoutResponse;
 				newLogout->set_username(message.logout_request().username());
 				responseMessage.set_allocated_logout_response(newLogout);
 
-				auto session = d_sessions.find(id);
-				if (session == d_sessions.end())
+				session->leave(username);
+				if (session->userCount() == 0)
 				{
+                    // I don't think there's any actual implementation needed here? 
+                    // because the server stays up
+					std::cout << "Erasing server session." << std::endl;
 					continue;
 				}
-				session->second->leave(username);
-				if (session->second->userCount() == 0)
-				{
-					std::cout << "Erasing session: " << session->second->id() << std::endl;
-					d_sessions.erase(session->second->id());
-					continue;
-				}
-				session->second->broadcast(responseMessage);
+				session->broadcast(responseMessage);
 			}
 			else
 			{
