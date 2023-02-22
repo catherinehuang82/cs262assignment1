a9a4f0df8af0894c1cca7892667aa95b9f2f0c62
Catherine Huang <38745832+catherinehuang82@users.noreply.github.com>
2023-02-22 14:03:00 -0500

    Update test_grpc.cc

--- grpc_chat/src/test_grpc.cc
+++ grpc_chat/src/test_grpc.cc
@@ -1,9 +1,20 @@
-#include <gtest/gtest.h>
 #include <string>
-#include "server.h"
 
 #include "chat.grpc.pb.h"
+#include "server.h"
 #include <string>
+#include "server.h"
+#include <string>
+#include <thread>
+#include <vector>
+#include <regex>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <termios.h>
+#include <unistd.h>
+#include <atomic>
+#include <assert.h>
 #include <thread>
 #include <vector>
 #include <regex>
@@ -18,42 +29,71 @@
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
 #include <grpcpp/security/server_credentials.h>
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
 
-void testListAccounts() {
+using chat::Chat;
+using chat::Message;
+using chat::ListAccountsRequest;
+using chat::DeleteAccountRequest;
+using chat::LogoutRequest;
+using chat::LoginRequest;
+using chat::LoginResponse;
+using chat::ListAccountsResponse;
+using chat::DeleteAccountResponse;
+using chat::LogoutResponse;
+using chat::MessageList;
+using chat::StreamRequest;
+using chat::StreamResponse;
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::ClientReader;
+using grpc::ClientReaderWriter;
+using grpc::ClientWriter;
+using grpc::Status;
+
+int main(int argc, char** argv) {
     Session *session;
-    std::string messageNew = "a";
-    std::string recipientUsername = "karly";
-    std::set<std::string> allAccounts = {"karly", "cat"};
+    // ClientContext context;
+    // std::unique_ptr<Chat::Stub> stub_;
+    // // ServerReaderWriter<StreamRequest, StreamResponse> *stream_cat;
+    // // ServerReaderWriter<StreamRequest, StreamResponse> *stream_karly;
 
-    StreamRequest message = createChatRequest(2, messageNew, recipientUsername);
-    ListAccountsResponse* response = session->listAccounts(message, allAccounts);
+    // set up
+    session->allAccounts.insert("karly");
+    session->allAccounts.insert("cat");
+    session->d_allStreams["karly"] = nullptr;
+    session->d_allStreams["cat"] = nullptr;
 
-    assert(response.username() == "karly");
-    assert(response.accounts() == "karly\ncat\n");
-}
+    // test accounts listing
+    ListAccountsRequest *listAccounts_request = new ListAccountsRequest();
+    listAccounts_request->set_username("cat");
+    listAccounts_request->set_wildcard("a");
 
-void testDeleteAccounts() {
-    std::string messageNew = "";
-    std::string recipientUsername = "karly";
+    StreamRequest message_listAccounts;
+    message_listAccounts.set_allocated_listaccounts_request(listAccounts_request);
 
-    StreamResponse responseMessage;
-    StreamRequest streamRequest;
-    ServerReaderWriter<StreamResponse, StreamRequest> s = (responseMessage, streamRequest);
+    ListAccountsResponse* listAccounts_response = session->listAccounts(message_listAccounts, session->allAccounts);
 
-    std::unordered_map<std::string, ServerReaderWriter<StreamResponse, StreamRequest> *> d_allStreams = {{"karly", s}, {"cat", s}};
-    std::set<std::string> allAccounts = {"karly", "cat"};
+    assert(listAccounts_response->username() == "cat");
+    assert(listAccounts_response->accounts() == "karly\ncat\n");
 
-    StreamRequest message = createChatRequest(3, messageNew, recipientUsername);
-    DeleteAccountResponse* response = deleteAccount(message, d_allStreams, allAccounts);
+    std::cout << "List accounts test passed.\n";
 
-    std::unordered_map<std::string, ServerReaderWriter<StreamResponse, StreamRequest> *> expected_d_allStreams = {{"cat", s}};
-    std::set<std::string> expected_allAccounts = {"cat"};
+    // test account deletion
+    DeleteAccountRequest *deleteAccount_request = new DeleteAccountRequest();
+    deleteAccount_request->set_username("cat");
 
-    assert(d_allStreams == expected_d_allStreams);
-    assert(allAccounts == expected_allAccounts);
-}
+    StreamRequest message_deleteAccount;
+    message_deleteAccount.set_allocated_deleteaccount_request(deleteAccount_request);
+    DeleteAccountResponse* deleteAccount_response = session->deleteAccount(message_deleteAccount, session->d_allStreams, session->allAccounts);
 
-int main(int argc, char **argv) {
-    testListAccounts();
-    testDeleteAccounts();
+    assert(deleteAccount_response->username() == "cat");
+    assert(deleteAccount_response->message() == "You have successfully deleted your account.");
+    assert(session->allAccounts.size() == 1);
+
+    std::cout << "All tests passed!\n";
 }
